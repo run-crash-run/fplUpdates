5f45bfee2c347cdd03e68b85f12b6dae7b2d4766
will <quigley.william@gmail.com>
2021-03-31 22:09:00 +0100

    Initial Commit

---
+++ src/main/java/com/fmlfpl/fplUpdates/FplUpdatesApplication.java
@@ -0,0 +1,13 @@
+package com.fmlfpl.fplUpdates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FplUpdatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FplUpdatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmlfpl/fplUpdates/FplUpdatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmlfpl.fplUpdates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FplUpdatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
